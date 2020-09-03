8e0f3b9e6bcebb8f5bdfa6eb678c930bbf1ccfd8
MrCowFace <59069516+MrCowFace@users.noreply.github.com>
2020-09-03 13:38:54 -0400

    added inputsytem
    some reason I am currently unable to inport UnityEngine.InputSystem to the code

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 10;
+
+    private Rigidbody rb;
+
+    private Vector3 movement;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+
+
+
+
+
+
+}
